62e234c7ead5f2f4467ce1c6330eb9a221b072a2
Mayank7371 <neprone.dev@gmail.com>
2025-09-03 20:00:34 +0530

    update files

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,2 +1,18 @@
-function App2() {}
+function App2() {
+  return (
+    <>
+      <div className="bg-slate-700 grid place-content-center max-w-full h-screen">
+        <button className="bg-sky-500 rounded-sm my-2 mx-2 py-1 px-1 text-white font-mono hover:bg-blue-950 hover:text-amber-300">
+          what is this?
+        </button>
+        <div className="bg-amber-400 hover:bg-amber-950 rounded-xl p-2">
+          <p className="text-white md:text-blue-950 md:bg-amber-400">
+            Lorem, ipsum dolor.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
 export default App2;
+// breakpoint in tailwindcss.com
