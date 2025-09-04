27e5b973cf6f58cf145bc75399561bd3e80b35ed
Mayank7371 <neprone.dev@gmail.com>
2025-09-04 23:30:37 +0530

    update and add app3

---
+++ src/App3.jsx
@@ -0,0 +1,24 @@
+function App3() {
+  return (
+    <>
+      <div className="bg-blue-950 grid place-content-center text-yellow-500 h-20 text-xl">
+        Jaipur
+      </div>
+      <div className="mt-3 bg-slate-600 h-screen">
+        <div className="max-w-sm mx-auto rounded-xl overflow-hidden md:max-w-xl bg-sky-800">
+          <div className="md:flex">
+            <div className="">
+              <img
+                className="md:h-72 md:w-full object-cover sm:h-full w-48"
+                src="https://images.pexels.com/photos/574337/pexels-photo-574337.jpeg"
+                alt="image"
+              />
+            </div>
+            <div className="m-4 text-sm text-white font-mono">content</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default App3;
